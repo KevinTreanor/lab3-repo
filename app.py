49c28a9cba0cc7c8d51bb3844da6fd0fcdae2211
KevinTreanor <c15337401@mydit.ie>
2017-10-04 16:46:57 +0100

    Update app.py

--- app.py
+++ app.py
@@ -6,9 +6,9 @@ app = Flask(__name__)
 # My SQL Instance configurations 
 # Change the HOST IP and Password to match your instance configurations
 app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = 'P@ssw0rd'
+app.config['MYSQL_PASSWORD'] = 'Lab3'
 app.config['MYSQL_DB'] = 'studentbook'
-app.config['MYSQL_HOST'] = '35.195.195.114'
+app.config['MYSQL_HOST'] = '130.211.81.243'
 mysql.init_app(app)
 
 # The first route to access the webservice from http://external-ip:5000/ 
@@ -22,3 +22,34 @@ def hello(): # Name of the method
 if __name__ == "__main__":
         app.run(host='0.0.0.0', port='5000') #Run the flask app at port 5000
 
+        
+ #Route to add to table
+@app.route("/")
+def add():
+    cur = mysql.connection.cursor()
+    cur.execute('''INSERT student1 INTO students''')
+    rv = cur.fetchall()
+    return str(rv)
+    if __name__ == "__main__":
+        app.run(host='0.0.0.0', port='5000')
+    
+    #Route to update table
+    @app.route("/")
+def update():
+    cur = mysql.connection.cursor()
+    cur.execute('''UPDATE students SET student1 = student2''')
+    rv = cur.fetchall()
+    return str(rv)
+    if __name__ == "__main__":
+        app.run(host='0.0.0.0', port='5000')
+    
+    #Route to delete from table
+    @app.route("/")
+def delete():
+    cur = mysql.connection.cursor()
+    cur.execute('''DELETE student1 FROM students''')
+    rv = cur.fetchall()
+    return str(rv)
+    if __name__ == "__main__":
+        app.run(host='0.0.0.0', port='5000')
+    
